fb60067e84466e2e9e4d2bd7fe521eb5d39c9ce0
Siddharth Gabu <gaburav567@gmail.com>
2021-03-13 18:48:04 +0530

    Add Source files

---
+++ Source/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+	io.stdout:setvbuf("no")
+	t.window.title = "PONG"
+
+end
---
+++ Source/main.lua
@@ -0,0 +1,184 @@
+push = require'push'
+Class = require 'class'
+
+require 'Paddle'
+require 'Ball'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+PADDLE_SPEED = 200
+
+function love.load()
+	love.graphics.setDefaultFilter('nearest','nearest')
+	--smallFont = love.graphics.newFont('font1.ttf',8)
+	--love.graphics.setFont(smallFont)
+	love.window.setTitle('Pong')
+	math.randomseed(os.time())
+	sounds ={
+		['edge_hit'] = love.audio.newSource('edge.wav','static'),
+		['paddle_hit'] = love.audio.newSource('paddle.wav','static'),
+		['score_hit'] = love.audio.newSource('score.wav','static')}
+	push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
+		fullscreen = false,
+		resizable = true,
+		vsync = true
+	})
+
+	player1 = Paddle(10,30,5,20)
+	player2 = Paddle(VIRTUAL_WIDTH - 10,VIRTUAL_HEIGHT - 30,5,20)
+	player1Score = 0
+	player2Score = 0
+	ball = Ball(VIRTUAL_WIDTH / 2 - 2,VIRTUAL_HEIGHT / 2 - 2,4,4)
+	gameState = 'start'
+end
+
+function love.resize(w,h)
+	push:resize(w,h)
+end
+
+function love.update(dt)
+	if gameState == 'serve' then
+		ball.dy = math.random(-50,50)
+		if servingPlayer == 1 then
+			ball.dx = math.random(140,200)
+		else
+			ball.dx = -math.random(140,200)
+		end
+	elseif gameState == 'play' then
+		if ball:collides(player1) then
+			ball.x = player1.x + 5
+			ball.dx = -ball.dx * 1.03
+			
+
+			if ball.dy < 0 then 
+				ball.dy = -math.random(10,150)
+			else
+				ball.dy = math.random(10,150)
+			end
+			sounds['paddle_hit']:play()
+		end
+		if ball:collides(player2) then 
+			ball.x = player2.x - 4
+			ball.dx = -ball.dx * 1.03
+			
+
+			if ball.dy < 0 then 
+				ball.dy = -math.random(10,150)
+			else
+				ball.dy = math.random(10,150)
+			end
+			sounds['paddle_hit']:play()
+		end
+		if ball.y <= 0 then
+			ball.y = 0
+			ball.dy = -ball.dy
+			sounds['edge_hit']:play()
+		end
+		if ball.y >= VIRTUAL_HEIGHT - 4 then
+			ball.y = VIRTUAL_HEIGHT - 4
+			ball.dy = -ball.dy
+			sounds['edge_hit']:play()
+		end
+		if ball.x > VIRTUAL_WIDTH - 4 then
+			servingPlayer = 2
+			player1Score = player1Score + 1
+			sounds['score_hit']:play()
+			if player1Score == 10 then
+				winningPlayer = 1
+				gameState = 'done'
+			else
+				ball:reset()
+				gameState = 'serve'
+			end
+		end
+		if ball.x < 0 then
+			servingPlayer = 1
+			player2Score = player2Score + 1
+			sounds['score_hit']:play()
+			if player2Score == 10 then
+				winningPlayer = 2
+				gameState = 'done'
+			else
+				ball:reset()
+				gameState = 'serve'
+			end
+
+		end
+	end
+
+	if love.keyboard.isDown('w') then
+		player1.dy = -PADDLE_SPEED
+	elseif love.keyboard.isDown('s') then
+		player1.dy = PADDLE_SPEED
+	else
+		player1.dy = 0
+	end
+
+	if love.keyboard.isDown('up') then
+		player2.dy = -PADDLE_SPEED
+	elseif love.keyboard.isDown('down') then
+		player2.dy = PADDLE_SPEED
+	else
+		player2.dy = 0
+	end
+
+	if gameState == 'play' then
+		ball:update(dt)
+	end
+
+	player1:update(dt)
+	player2:update(dt)
+end
+
+function love.keypressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	elseif key == 'enter' or key == 'return' then
+		if gameState == 'start' then
+			gameState = 'serve'
+		elseif gameState == 'serve' then
+			gameState = 'play'
+		elseif gameState == 'done' then
+			gameState = 'serve'
+			ball:reset()
+			player1Score = 0
+			player2Score = 0
+			if winningPlayer == 1 then
+				servingPlayer = 2
+			else
+				servingPlayer = 1
+			end
+		end
+	end
+end
+
+function love.draw()
+	push:apply('start')
+	--love.graphics.clear(40,45,52,255)
+	--love.graphics.printf('Hello pong!',0,5,VIRTUAL_WIDTH,'center')
+	if gameState == 'start' then
+		love.graphics.printf('Hello Start State!',0,20,VIRTUAL_WIDTH,'center')
+	elseif gameState == 'serve' then
+		love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve",0,10,VIRTUAL_WIDTH,'center')
+		love.graphics.printf('Press Enter to serve!',0,20,VIRTUAL_WIDTH,'center')
+	elseif gameState == 'Play' then
+		love.graphics.printf('Hello Play State!',0,20,VIRTUAL_WIDTH,'center')
+	elseif gameState == 'done' then
+		love.graphics.printf('Player ' .. tostring(winningPlayer) .. 'wins!',0,10,VIRTUAL_WIDTH,'center')
+		love.graphics.printf('Press Enter to restart!',0,20,VIRTUAL_WIDTH,'center')
+	end
+	love.graphics.print(tostring(player1Score),VIRTUAL_WIDTH / 2 - 50,VIRTUAL_HEIGHT/3)
+	love.graphics.print(tostring(player2Score),VIRTUAL_WIDTH / 2 + 30,VIRTUAL_HEIGHT/3)
+	player1:render()
+	player2:render()
+	ball:render()
+	displayFPS()
+	push:apply('end')
+end
+
+function displayFPS()
+	love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()),10,10)
+end
